1da8c1dcb6ce5ebf2f7ad479097862fa5e1a8c88
charith85 <kalpa.fit6@gmail.com>
2014-09-06 14:35:36 +0530

    DB Validation Part completed

--- src/Login/LoginBundle/Controller/DefaultController.php
+++ src/Login/LoginBundle/Controller/DefaultController.php
@@ -6,8 +6,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('LoginLoginBundle:Default:index.html.twig', array('name' => $name));
+        $userName = "Charith1";
+        $password = "password";
+        
+        $em = $this->getDoctrine()->getEntityManager();
+        $repository = $em ->getRepository('LoginLoginBundle:Users');
+        
+        $user = $repository ->findOneBy(array('userName'=>$userName,'password'=>$password));
+        
+        if($user) 
+        {
+             return $this->render('LoginLoginBundle:Default:index.html.twig', array('name' => $user->getFirstName()));
+            
+        }
+        else 
+        {
+            
+             return $this->render('LoginLoginBundle:Default:index.html.twig', array('name' => 'Login Failed'));
+            
+        }
+        
+        
+       
     }
 }
